94ad9930be80ea13be6dae24983d376fcdb6cb31
Jullivasx <jullivasx@gmail.com>
2022-05-09 09:05:12 +0300

    first test

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 
-const node = require('node-fetch');
+const fetch = require('node-fetch');
+
 
 const app = express();
 
+app.use(express.json());
 app.use(express.static('public'));
+
+app.post('/fetch', async (req, res) => {
+    try {
+        console.log('POST: /fetch ' + JSON.stringify(req.body));
+        const url = req.body.url;
+        const response = await fetch(url);
+        const body = await response.text()
+        res.json({
+            html: body,
+        });
+    } catch (error) {
+        console.error(error);
+        res.sendStatus(505);
+    }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
